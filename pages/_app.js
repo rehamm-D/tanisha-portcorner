fc6df9cca7519ae156c25d14b0d4400fa7df098e
Tanisha Hasan <talkwithtinaa34@gmail.com>
2025-07-20 20:42:30 +0530

    Update globals.css with favicon to the website

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Tanisha Hasan – Portfolio</title>
+        <meta name="description" content="Crafting human-centered AI and digital experiences." />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
